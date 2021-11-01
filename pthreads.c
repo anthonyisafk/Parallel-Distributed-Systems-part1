037f92d702ed002c4b688e8610ab2bc7fc434d86
anthonyisafk <tonytonytony555@live.com>
2021-11-01 04:06:16 +0200

    Update pthreads.c

--- pthreads.c
+++ pthreads.c
@@ -1,3 +1,15 @@
+/*
+ * pthreads.c 
+ * Convert a square N x N matrix into the CSR format, made for sparse matrices:
+ * https://en.wikipedia.org/wiki/Sparse_matrix#Compressed_sparse_column_(CSC_or_CCS)
+ * 
+ * Authors: Antonios Antoniou - 9482
+ *          Efthymios Grigorakis - 9694
+ *    
+ * 2021 Aristotle University of Thessaloniki
+ * Parallel and Distributed Systems.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -5,8 +17,7 @@
 #define SIZE 15
 
 // A struct used to turn sparse matrices to CSR data structures.
-// The notation and algorithm used is taken directly from the given Wikipedia page:
-// https://en.wikipedia.org/wiki/Sparse_matrix#Compressed_sparse_column_(CSC_or_CCS)
+// The notation and algorithm used is taken directly from the given Wikipedia page.
 typedef struct {
   long size;
   int *values;
@@ -21,9 +32,10 @@ int **makeRandomSparseTable(int size) {
     table[i] = (int *) malloc(size * sizeof(int));
   }
 
+  // Add a 1 to the table with a 136/1000 probability.
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < size; j++) {
-      table[i][j] = (rand() % 100 > 13) ? 0 : 1;
+      table[i][j] = (rand() % 1000 > 136) ? 0 : 1;
     }
   }
 
@@ -31,9 +43,9 @@ int **makeRandomSparseTable(int size) {
 }
 
 
-void printTable(int **table) {
-  for (int i = 0; i < SIZE; i++) {
-    for (int j = 0; j < SIZE; j++) {
+void printTable(int **table, int size) {
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
       printf(" %d ", table[i][j]);
     }
     printf("\n");
@@ -46,35 +58,39 @@ void printTable(int **table) {
 // Converts a sparse matrix to CSR.
 csr convertTOCSR(int **table, long size) {
   // Keep track of the nonzero objects.
-  long nonzeros; 
-  // Initialize the 3 necessary arrays.
+  long nonzeros = 0; 
+
+  // Initialize the 3 necessary arrays. row_index has a standard length of "rows+1"
+  long *rowIndex = (long *) malloc((size+1) * sizeof(long));
+  // values and col_index have a length of the total nonzero values.
   int *values = (int *) malloc(100 * sizeof(int));
   long *colIndex = (long *) malloc(100 * sizeof(long));
-  long *rowIndex = (long *) malloc((size+1) * sizeof(long));
   
   for (long i = 0; i < size; i++) {
     // Add the nonzero values that are placed above the current row. 
-    rowIndex[i] = (long) nonzeros;
+    rowIndex[i] = nonzeros;
 
     for (long j = 0; j < size; j++) {
       if (table[i][j] != 0) {
         nonzeros++;
 
         // Make sure to extend the arrays in case their size exceeds the current one.
-        if (nonzeros != 0 && (nonzeros % 100 == 0)) {
+        if (nonzeros % 100 == 0) {
+          printf("%d\n", table[i][j]);
+
           values = (int *) realloc(values, (nonzeros+100) * sizeof(int));
           colIndex = (long *) realloc(colIndex, (nonzeros+100) * sizeof(long));
         }
 
         // Add the nonzero value and the respective column index.
         values[nonzeros-1] = table[i][j];
-        colIndex[nonzeros-1] = (long) j; 
+        colIndex[nonzeros-1] = j; 
       }
     }
 
     // Add the last value before exiting the for loop.
     if (i == size - 1) {
-      rowIndex[size] = (long) nonzeros;
+      rowIndex[size] = nonzeros;
     }
   }
 
@@ -115,15 +131,29 @@ int **matmul (int **table1, int **table2, int size) {
 
 int main(int argc, char **argv) {
 
+  // Initialize a random sparse matrix.
   int **random1 = makeRandomSparseTable(SIZE);
-  int **random2 = makeRandomSparseTable(SIZE);
-  printTable(matmul(random1, random1, SIZE));
+  printTable(random1, SIZE);
 
-  // csr converted = convertTOCSR(random, (long)SIZE);
+  // Try to convert the table to CSR. (Finally works)
+  csr converted = convertTOCSR(random1, (long)SIZE);
+  long nonzeros = converted.rowIndex[SIZE];
 
+  // Print the resulting arrays, to test the result.
+  for (int i = 0; i < nonzeros; i++) {
+    printf(" %d ", converted.values[i]);
+  }
+  printf("\n");
 
+  for (int i = 0; i < nonzeros; i++) {
+    printf(" %ld ", converted.colIndex[i]);
+  }
+  printf("\n");
 
+  for (int i = 0; i < SIZE+1; i++) {
+    printf(" %ld ", converted.rowIndex[i]);
+  }
+  printf("\n");
   
-
   return 0;
 }
